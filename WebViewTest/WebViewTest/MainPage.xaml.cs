f3070149456d960c602b231cbf25b7c74b5cfc8e
Nico Zhu <v-zhumin@microsoft.com>
2018-01-09 16:36:50 +0800

    Add project files.

---
+++ WebViewTest/WebViewTest/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebViewTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Mybtn.Clicked += Mybtn_Clicked;
+        }
+
+        private void Mybtn_Clicked(object sender, EventArgs e)
+        {
+            var browser = new WebView { HeightRequest = 500 };
+            browser.SetBinding(WebView.SourceProperty, "UrlSource");
+            browser.SetBinding(WebView.IsVisibleProperty, "IsWebViewVisible");
+
+            Root.Children.Add(browser);
+
+            var ViewModel = BindingContext as MainPageViewModel;
+            ViewModel.IsWebViewVisible = true;
+            //   MyWebView.Source = "https://www.cnet.com";
+        }
+    }
+}
---
+++ WebViewTest/WebViewTest/MainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WebViewTest
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainPageViewModel()
+        {
+        }
+
+        private bool isWebViewVisible = true;
+
+        public bool IsWebViewVisible
+        {
+            get { return isWebViewVisible; }
+            set
+            {
+                isWebViewVisible = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string urlSource = "https://www.cnet.com";
+
+        public string UrlSource
+        {
+            get
+            {
+                return urlSource;
+            }
+            set
+            {
+                urlSource = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
